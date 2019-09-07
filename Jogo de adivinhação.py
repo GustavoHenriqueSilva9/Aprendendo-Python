10e004044e103e0a97cc8380449eea2f48078f01
unknown <gustavohenriquesilva124@gmail.com>
2019-09-07 12:28:57 -0300

    atualizando jogo de adivinhação

--- Jogo de adivinhação.py
+++ Jogo de adivinhação.py
@@ -3,8 +3,49 @@ print("Bem vindo ao Jogo de Adivinhação!")
 print("#####################################")
 import random
 numero_adivinhar = random.randrange(1,100)
-total_tentativas = int(input("Digite o Numero de Tentativas para o Jogador Tentar Acertar: "))
-print("=====================================================================")
+print("Niveis de Dificuldade = (1)Facil, (2)Medio, (3)Dificil, (4)Muito Dificil")
+dificuldade_jogo = int(input("Digite O Nivel de Dificuldade: "))
+if (dificuldade_jogo == 1):
+    total_tentativas = 10
+    print("Nivel Facil Selecionado com Total de 10 Tentativas")
+    print("------------------------------------------------------------")
+elif (dificuldade_jogo == 2):
+    total_tentativas = 6
+    print("Nivel Medio Selecionado com Total de 6 Tentativas")
+    print("------------------------------------------------------------")
+elif (dificuldade_jogo == 3):
+    total_tentativas = 4
+    print("Nivel Dificil Selecionado com Total de 4 Tentativas")
+    print("------------------------------------------------------------")
+elif (dificuldade_jogo == 4):
+    total_tentativas = 2
+    print("Nivel Muito Dificil Selecionado com Total de 2 Tentativas")
+    print("------------------------------------------------------------")
+else:
+    while ((dificuldade_jogo > 4) or (dificuldade_jogo < 1)):
+        print("------------------------------------------------------------")
+        print("Niveis de Dificuldade, (1)Facil, (2)Medio, (3)Dificil, (4)Muito Dificil")
+        dificuldade_jogo = int(input("Digite um Nivel de Dificuldade Valido: "))
+        if (dificuldade_jogo == 1):
+            total_tentativas = 10
+            print("Nivel Facil Selecionado com Total de 10 Tentativas")
+            print("------------------------------------------------------------")
+            break
+        elif (dificuldade_jogo == 2):
+            total_tentativas = 6
+            print("Nivel Medio Selecionado com Total de 6 Tentativas")
+            print("------------------------------------------------------------")
+            break
+        elif (dificuldade_jogo == 3):
+            total_tentativas = 4
+            print("Nivel Dificil Selecionado com Total de 4 Tentativas")
+            print("------------------------------------------------------------")
+            break
+        elif (dificuldade_jogo == 4):
+            total_tentativas = 2
+            print("Nivel Muito Dificil Selecionado com Total de 2 Tentativas")
+            print("------------------------------------------------------------")
+            break
 for i in range (0,total_tentativas):
     chute = int(input("Chute um Número:  "))
     if(chute < 1 or chute > 100):
